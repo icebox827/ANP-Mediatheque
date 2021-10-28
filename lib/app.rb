9290c6c2de9a37fc00f09f118092a7e60738e47f
Denis <icebox827@gmail.com>
2021-10-28 13:01:18 -0400

    Update app with new method

--- lib/app.rb
+++ lib/app.rb
@@ -48,6 +48,48 @@ class App
       puts "Person created successfully"
   end
 
+  def create_book
+    print "Enter title: " 
+    title = gets.chomp 
+    print "Enter author: "
+    author = gets.chomp
+    @books << Book.new(title, author)
+    puts "Book created successfully"
+  end
+
+  def create_rental 
+    puts "Select a book from the following list by number"
+    @books.each_with_index do |book, index| 
+      puts "#{index} Title: \"#{book.title}\", Author: #{book.author}"
+    end
+
+    book_index = gets.chomp.to_i
+
+    puts "\nSelect a person from the following list by number (not id)"
+    @people.each_with_index do |person, index|
+      puts "#{index} [#{person.class}] Name: #{person.name}, ID: #{person.id}, NIN: #{person.nin}"
+    end 
+
+    person_index = gets.chomp.to_i
+
+    print "\nDate:"
+
+    @rentals << Rental.new(date, @books[book_index], @people[person_index])
+    puts 'Rental created successfully'
+  end
+
+  def list_rentals_for_person_id 
+    puts "Id of person: "
+    id = gets.chomp.to_i
+
+    rentals = @rentals.filter { |rental| rental.person.id == id }
+
+    puts "Rentals: "
+    rentals.each do |rental|
+      puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
+    end 
+  end
+
   private 
 
   def create_student(nin, full_name)
@@ -58,7 +100,7 @@ class App
   end
 
   def create_teacher(nin, full_name) 
-    print "Plase enter course: " 
+    print "Please enter course: " 
     course = gets.chomp
     Teacher.new(nin, full_name, course)
   end
